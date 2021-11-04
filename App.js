2e6138ae2e85bed4d4b05b5b72b08a361b7f8640
Kyulol <sivan.jeremie@gmail.com>
2021-11-04 15:40:21 +0100

    start design cards

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
-import tw from 'tailwind-react-native-classnames';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaView style={tw`h-full bg-white`}>
-        <View style={tw`p-5`}>
-          <Image 
-            style={{width: 100, height: 100, resizeMode: "contain"}}
-            source={{uri: "https://links.papareact.com/gzs",}}
-          />
-        </View>
+      <SafeAreaProvider>
         <HomeScreen />
-      </SafeAreaView>
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, SafeAreaView, Image } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Here is the Homescreen</Text>
-    </View>
+    <SafeAreaView style={tw`h-full bg-white`}>
+      <View style={tw`p-5`}>
+        <Image 
+          style={{width: 100, height: 100, resizeMode: "contain"}}
+          source={{uri: "https://links.papareact.com/gzs",}}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
   )
 }
 
