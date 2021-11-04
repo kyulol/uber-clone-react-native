87447137dba5f14648d45210b93c66a167973741
Kyulol <sivan.jeremie@gmail.com>
2021-11-04 01:02:20 +0100

    Init react native + tailwind and toolkit redux

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import tw from 'tailwind-react-native-classnames';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaView style={tw`h-full bg-white`}>
+        <View style={tw`p-5`}>
+          <Image 
+            style={{width: 100, height: 100, resizeMode: "contain"}}
+            source={{uri: "https://links.papareact.com/gzs",}}
+          />
+        </View>
+        <HomeScreen />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'red',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
